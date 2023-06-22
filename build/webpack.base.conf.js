776d515e7eb659e3417f423348ff6cfe767cdfc5
Francois Buys <buys.fran@gmail.com>
2023-06-22 15:19:41 +0200

    Step 4: Fix compile time issues
    All issues were related to webpack in this case.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,67 +1,78 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
+var config = require("../config");
+var path = require("path");
+var utils = require("./utils");
+var vueLoaderConfig = require("./vue-loader.conf");
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+  return path.join(__dirname, "..", dir);
 }
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: "./src/main.js",
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    filename: "[name].js",
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: [".js", ".vue", ".json"],
     alias: {
-      'vue': '@vue/compat',
-      '@': resolve('src')
-    }
+      vue: "@vue/compat",
+      "@": resolve("src"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
+        loader: "eslint-loader",
+        enforce: "pre",
+        include: [resolve("src"), resolve("test")],
         options: {
-          formatter: require('eslint-friendly-formatter')
-        }
+          formatter: require("eslint-friendly-formatter"),
+        },
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
+        loader: "vue-loader",
+        options: vueLoaderConfig,
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        loader: "babel-loader",
+        include: [resolve("src"), resolve("test")],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("img/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
-  }
-}
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]"),
+        },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true, // enable/disable Options API support
+      __VUE_PROD_DEVTOOLS__: false // enable/disable devtools support in production
+    }),
+  ],
+};
